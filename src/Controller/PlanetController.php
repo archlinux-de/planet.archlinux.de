ec3d3d1baa0739a3dfb2588c7f82940434f7e668
Pierre Schmitz <pierre@archlinux.de>
2019-01-04 22:18:39 +0100

    Export item feeds

--- src/Controller/PlanetController.php
+++ src/Controller/PlanetController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Repository\FeedRepository;
 use App\Repository\ItemRepository;
+use App\Service\ItemExporter;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Cache;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -17,19 +18,28 @@ class PlanetController extends AbstractController
     /** @var FeedRepository */
     private $feedRepository;
 
+    /** @var ItemExporter */
+    private $itemExporter;
+
     /**
      * @param ItemRepository $itemRepository
      * @param FeedRepository $feedRepository
+     * @param ItemExporter $itemExporter
      */
-    public function __construct(ItemRepository $itemRepository, FeedRepository $feedRepository)
-    {
+    public function __construct(
+        ItemRepository $itemRepository,
+        FeedRepository $feedRepository,
+        ItemExporter $itemExporter
+    ) {
         $this->itemRepository = $itemRepository;
         $this->feedRepository = $feedRepository;
+        $this->itemExporter = $itemExporter;
     }
 
     /**
      * @Route("/", methods={"GET"})
      * @Cache(smaxage="600")
+     *
      * @return Response
      */
     public function indexAction(): Response
@@ -40,6 +50,21 @@ class PlanetController extends AbstractController
                 'items' => $this->itemRepository->findLatest(30),
                 'feeds' => $this->feedRepository->findLatest()
             ]
+        );
+    }
+
+    /**
+     * @Route("/{_format}.xml", methods={"GET"}, requirements={"_format": "atom|rss"})
+     * @Route("/rss20.xml", methods={"GET"}, defaults={"_format"="rss"})
+     * @Cache(smaxage="600")
+     *
+     * @param string $_format
+     * @return Response
+     */
+    public function feedAction(string $_format): Response
+    {
+        return new Response(
+            $this->itemExporter->export($this->itemRepository->findLatest(30), $_format)
         );
     }
 }
---
+++ src/Service/ItemExporter.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Service;
+
+use App\Repository\ItemRepository;
+use Exercise\HTMLPurifierBundle\HTMLPurifiersRegistryInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Zend\Feed\Writer\Feed;
+
+class ItemExporter
+{
+    /** @var ItemRepository */
+    private $itemRepository;
+
+    /** @var UrlGeneratorInterface */
+    private $urlGenerator;
+
+    /** @var HTMLPurifiersRegistryInterface */
+    private $purifiersRegistry;
+
+    /**
+     * @param ItemRepository $itemRepository
+     * @param UrlGeneratorInterface $urlGenerator
+     * @param HTMLPurifiersRegistryInterface $purifiersRegistry
+     */
+    public function __construct(
+        ItemRepository $itemRepository,
+        UrlGeneratorInterface $urlGenerator,
+        HTMLPurifiersRegistryInterface $purifiersRegistry
+    ) {
+        $this->itemRepository = $itemRepository;
+        $this->urlGenerator = $urlGenerator;
+        $this->purifiersRegistry = $purifiersRegistry;
+    }
+
+    /**
+     * @param array $items
+     * @param string $format
+     * @return string
+     */
+    public function export(array $items, string $format): string
+    {
+        $feed = new Feed();
+        $feed->setTitle('Arch Linux Planet')
+            ->setDateModified()
+            ->setId(
+                $this->urlGenerator->generate(
+                    'app_planet_feed',
+                    ['_format' => $format],
+                    UrlGeneratorInterface::ABSOLUTE_URL
+                )
+            )
+            ->setLink(
+                $this->urlGenerator->generate(
+                    'app_planet_index',
+                    [],
+                    UrlGeneratorInterface::ABSOLUTE_URL
+                )
+            )
+            ->setFeedLink(
+                $this->urlGenerator->generate(
+                    'app_planet_feed',
+                    ['_format' => $format],
+                    UrlGeneratorInterface::ABSOLUTE_URL
+                ),
+                $format
+            )
+            ->setDescription('planet.archlinux.de');
+
+        foreach ($items as $item) {
+            $entry = $feed->createEntry();
+
+            $source = $entry->createSource();
+            $source
+                ->setTitle($item->getFeed()->getTitle())
+                ->setLink($item->getFeed()->getLink())
+                ->setDateModified($item->getFeed()->getLastModified())
+                ->setFeedLink($item->getFeed()->getUrl(), 'atom');
+
+            $entry
+                ->setLink($item->getLink())
+                ->setTitle(html_entity_decode($item->getTitle()))
+                ->setDescription($this->purifiersRegistry->get('planet')->purify($item->getDescription()))
+                ->setDateModified($item->getLastModified())
+                ->setSource($source);
+
+            if ($format == 'atom') {
+                if ($item->getAuthor()->getName()) {
+                    $entry->
+                    addAuthor([
+                        'name' => $item->getAuthor()->getName(),
+                        'uri' => $item->getAuthor()->getUri() ?? $item->getFeed()->getLink()
+                    ]);
+                } else {
+                    $entry->addAuthor(['name' => $item->getFeed()->getTitle()]);
+                };
+            }
+
+            $feed->addEntry($entry);
+        }
+
+        return $feed->export($format);
+    }
+}
