37655218f4c9ebaa27588bbb583246e2d3346b96
Pierre Schmitz <pierre@archlinux.de>
2020-09-05 08:00:24 +0200

    Update dependencies

--- api/src/Repository/FeedRepository.php
+++ api/src/Repository/FeedRepository.php
@@ -4,7 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Feed;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\Persistence\ManagerRegistry;
 use Doctrine\ORM\Tools\Pagination\Paginator;
 
 class FeedRepository extends ServiceEntityRepository
