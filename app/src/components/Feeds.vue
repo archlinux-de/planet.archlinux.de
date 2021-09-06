d0893f0964786756531d173c7e41bc305b60355e
Pierre Schmitz <pierre@archlinux.de>
2021-09-06 20:53:25 +0200

    Add placeholders

--- app/src/components/Feeds.vue
+++ app/src/components/Feeds.vue
@@ -1,7 +1,15 @@
 <template>
-  <ul class="ps-4">
-    <li :key="id" v-for="(feed, id) in feeds"><a :href="feed.link" :title="feed.description">{{ feed.title }}</a></li>
-  </ul>
+  <div>
+    <template v-if="feeds.length === 0">
+      <ul class="ps-4 placeholder-glow" aria-hidden="true">
+        <li :key="'placeholder-'+id" v-for="id in 9" class="placeholder col-8 bg-primary"></li>
+      </ul>
+    </template>
+
+    <ul class="ps-4">
+      <li :key="id" v-for="(feed, id) in feeds"><a :href="feed.link" :title="feed.description">{{ feed.title }}</a></li>
+    </ul>
+  </div>
 </template>
 
 <script>
@@ -27,8 +35,11 @@ export default {
           limit: this.limit,
           offset: this.offset
         })
-        .then(data => { this.feeds = data.items })
-        .catch(() => { })
+        .then(data => {
+          this.feeds = data.items
+        })
+        .catch(() => {
+        })
     }
   },
   mounted () {
--- app/src/components/Items.vue
+++ app/src/components/Items.vue
@@ -1,5 +1,24 @@
 <template>
   <div>
+    <template v-if="data.items.length === 0">
+      <div class="mb-5 placeholder-glow" :key="'placeholder-' + id" v-for="id in 10" aria-hidden="true">
+        <div
+          class="d-lg-flex justify-content-between align-items-baseline border border-top-0 border-start-0 border-end-0 mb-2">
+          <h2 class="p-0 text-break placeholder col-10 bg-primary"></h2>
+          <div class="p-0 placeholder col-1"></div>
+        </div>
+        <div class="item-description text-break mw-100">
+          <p :key="'placeholder--p-' + id" v-for="id in 4"><span class="placeholder col-12"></span>
+            <span class="placeholder col-12"></span>
+            <span class="placeholder col-12"></span>
+            <span class="placeholder col-4"></span></p>
+        </div>
+        <div class="fst-italic">
+          <span class="placeholder col-2"></span>
+        </div>
+      </div>
+    </template>
+
     <div class="mb-5" :key="id" v-for="(item, id) in data.items">
       <div
         class="d-lg-flex justify-content-between align-items-baseline border border-top-0 border-start-0 border-end-0 mb-2">
