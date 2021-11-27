16ac8976381d48442057054e797cbf8db81132cc
Pierre Schmitz <pierre@archlinux.de>
2021-11-27 09:59:05 +0100

    Update DBAL

--- api/migrations/Version20190101125607.php
+++ api/migrations/Version20190101125607.php
@@ -14,11 +14,6 @@ final class Version20190101125607 extends AbstractMigration
 {
     public function up(Schema $schema): void
     {
-        $this->abortIf(
-            $this->connection->getDatabasePlatform()->getName() !== 'mysql',
-            'Migration can only be executed safely on \'mysql\'.'
-        );
-
         $this->addSql('CREATE TABLE item (public_id VARCHAR(191) NOT NULL, feed_url VARCHAR(191) NOT NULL, title VARCHAR(255) NOT NULL, description LONGTEXT NOT NULL, last_modified DATETIME NOT NULL, link VARCHAR(255) NOT NULL, author_name VARCHAR(255) DEFAULT NULL, author_uri VARCHAR(255) DEFAULT NULL, INDEX IDX_1F1B251E6FAD93B (feed_url), INDEX IDX_1F1B251E270A2932 (last_modified), PRIMARY KEY(public_id, feed_url)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE feed (url VARCHAR(191) NOT NULL, title VARCHAR(255) NOT NULL, description VARCHAR(255) DEFAULT NULL, last_modified DATETIME NOT NULL, link VARCHAR(255) NOT NULL, PRIMARY KEY(url)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE item ADD CONSTRAINT FK_1F1B251E6FAD93B FOREIGN KEY (feed_url) REFERENCES feed (url) ON DELETE CASCADE');
@@ -26,11 +21,6 @@ final class Version20190101125607 extends AbstractMigration
 
     public function down(Schema $schema): void
     {
-        $this->abortIf(
-            $this->connection->getDatabasePlatform()->getName() !== 'mysql',
-            'Migration can only be executed safely on \'mysql\'.'
-        );
-
         $this->addSql('ALTER TABLE item DROP FOREIGN KEY FK_1F1B251E6FAD93B');
         $this->addSql('DROP TABLE item');
         $this->addSql('DROP TABLE feed');
--- api/migrations/Version20191217125634.php
+++ api/migrations/Version20191217125634.php
@@ -11,11 +11,6 @@ final class Version20191217125634 extends AbstractMigration
 {
     public function up(Schema $schema): void
     {
-        $this->abortIf(
-            $this->connection->getDatabasePlatform()->getName() !== 'mysql',
-            'Migration can only be executed safely on \'mysql\'.'
-        );
-
         $this->addSql('ALTER TABLE item DROP PRIMARY KEY');
         $this->addSql('ALTER TABLE item DROP public_id, CHANGE feed_url feed_url VARCHAR(191) DEFAULT NULL');
         $this->addSql('ALTER TABLE item ADD PRIMARY KEY (link)');
@@ -23,11 +18,6 @@ final class Version20191217125634 extends AbstractMigration
 
     public function down(Schema $schema): void
     {
-        $this->abortIf(
-            $this->connection->getDatabasePlatform()->getName() !== 'mysql',
-            'Migration can only be executed safely on \'mysql\'.'
-        );
-
         $this->addSql('ALTER TABLE item DROP PRIMARY KEY');
         $this->addSql(
             'ALTER TABLE item ADD public_id VARCHAR(191) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE feed_url feed_url VARCHAR(191) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`'
