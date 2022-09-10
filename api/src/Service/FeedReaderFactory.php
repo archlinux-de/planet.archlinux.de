b772f979e68768b48ff1a5f84126c60e928b9546
Pierre Schmitz <pierre@archlinux.de>
2022-09-10 12:21:44 +0200

    Mitigate issues with some HTTP/2 servers

--- api/src/Service/FeedReaderFactory.php
+++ api/src/Service/FeedReaderFactory.php
@@ -11,6 +11,7 @@ class FeedReaderFactory
         $feedReader->enable_cache(false);
         $feedReader->enable_exceptions(true);
         $feedReader->set_useragent('planet.archlinux.de/1.0');
+        $feedReader->set_curl_options([CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1]);
         $feedReader->init();
 
         return $feedReader;
